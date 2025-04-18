6e360d6275ed2611ea88d695e7f99eebe9470312
YEX01 <vortex8934@gmail.com>
2025-04-18 14:29:21 +0530

    Update config.py

--- Youtube/config.py
+++ Youtube/config.py
@@ -1,9 +1,11 @@
 import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 class Config(object):
-     
     BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
-    API_ID = int(os.environ.get("API_ID", ))
+    API_ID = int(os.environ.get("API_ID", 0)) 
     API_HASH = os.environ.get("API_HASH", "")
     CHANNEL = os.environ.get("CHANNEL", "")
-    HTTP_PROXY = ''
+    HTTP_PROXY = os.environ.get("HTTP_PROXY", "")  # Fixed typo from HTTP_PROXY to HTTP_PROXY?
